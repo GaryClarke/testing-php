78c61b89c9b876add3ed4b04fcc20bf87f04e939
Gary Clarke <GaryClarke@users.noreply.github.com>
2023-05-08 21:50:54 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ require __DIR__ . '/../vendor/autoload.php';
 
 const URL = 'localhost';
 
-const BEARER_TOKEN = 'Bearer AAAAAAAAAAAAAAAAAAAAAGOHWgEAAAAAyAlJNNOg5JawlVzbJjSqn3YFcRY%3DhOBStxksrvQRkOvQG2deKz8E2R9WShPAz3mDEm4rEDFWVHt3Ez';
+const BEARER_TOKEN = 'Bearer jshaskjdhakdh';
